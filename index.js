04b7e565025d4b8c34e8a47075b01254e653fff7
Jason Yu <jason.yu14@imperial.ac.uk>
2016-10-31 16:05:05 +0000

    set up body parser and mongoose

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var config = require('./lib/config');
+console.log(`>> valid user ids: ${JSON.stringify(config.valid_user_ids)}`);
+
+/* Initiate the user data in the database */
+require('./lib/utils/init_user_db')(config.valid_user_ids);
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-/* connect to mongodb */
-mongoose.connect(config.db_uri);
+/* body parser */
+app.use(require('body-parser').json());
 
 /* logger */
 app.use(require('express-log-url'));
+
+/* connect to mongodb */
+mongoose.connect(config.db_uri);
  
 /* routers */
 app.use(require('./lib/routers'));
